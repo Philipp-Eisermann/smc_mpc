99c4eba6ab26603aef426224520c921c8cb943e8
Philipp Eisermann <>
2022-05-18 23:11:15 +0200

    init

---
+++ helpers/inputgen.py
@@ -0,0 +1,24 @@
+#! /usr/bin/python
+# Construct created by JAcob Obrman
+from random import randint, seed
+import sys
+
+def get_random_set(n: int):
+        rand_set = set()
+        while len(rand_set) < n:
+                rand_set.add(randint(1, 3*n))
+        return rand_set
+
+def set_to_string(s: set):
+        return ' '.join(map(str, s))
+
+try:
+        n = int(sys.argv[1])
+        seed(int(sys.argv[2]))
+except:
+        print("Usage: ./inputgen.py <amount> <seed>")
+        print("Print <amount> many random numbers in range [1:3*<amount>]")
+        exit()
+
+print(set_to_string(get_random_set(n)))
+
